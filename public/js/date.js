19806d6144bf7934f4d9948cc8974f968f2d555e
Juan Riglos <juanriglos@hotmail.com>
2016-05-18 13:39:13 -0300

    Anda el navbar

---
+++ public/js/date.js
@@ -0,0 +1,10 @@
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+
+var yyyy = today.getFullYear();
+if(dd<10){dd='0'+dd} if(mm<10){mm='0'+mm} today = mm+'/'+dd+'/'+yyyy;
+
+$('#theDate').attr('value', today);
+
+alert($('#theDate').attr('value'));
